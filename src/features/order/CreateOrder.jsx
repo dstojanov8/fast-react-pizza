b6786c798c0f8801962ad873750b58a1ba05252e
Dimitrije <dstojanovga@gmail.com>
2025-03-07 11:36:16 +0100

    Added Redux store with user Reducer

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
+import { useSelector } from "react-redux";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -34,7 +35,9 @@ const fakeCart = [
 ];
 
 function CreateOrder() {
-  //* Here we wan't to display application 'submitting' state
+  const username = useSelector((state) => state.user.username);
+
+  //* Here we want to display application 'submitting' state
   const navigation = useNavigation();
   const isSubmitting = navigation.state === "submitting";
 
@@ -55,7 +58,16 @@ function CreateOrder() {
       <Form method="POST">
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
-          <input className="input grow" type="text" name="customer" required />
+          <input
+            className="input grow"
+            type="text"
+            name="customer"
+            //* Here we use 'defaultValue' instead of 'value' since it is not local state
+            //* but global state with no change event listeners. This way we can chane it.
+            //* We can't make this a controlled element.
+            defaultValue={username}
+            required
+          />
         </div>
 
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
